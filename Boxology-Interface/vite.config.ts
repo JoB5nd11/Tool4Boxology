14012b77fe00fa88c383ab90fc6f2e56b6844eb4
mahsa.forghani <xx.xxxx@mail.com>
2025-08-18 14:48:23 +0200

    add logo and web title

--- Boxology-Interface/vite.config.ts
+++ Boxology-Interface/vite.config.ts
@@ -1,16 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',
-  server: {
-    port: 5173,
-    host: '0.0.0.0',
-    open: true
-  },
+  publicDir: 'public',
   build: {
-    outDir: 'dist',
-    sourcemap: true
+    outDir: 'dist'
   }
 })
